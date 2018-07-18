62e192018869586fbb63c6e81d7fbb622e3831a9
1 <ppark9553@gmail.com>
2018-07-18 11:26:18 +0900

    도커 컴포즈 버전 수정 세팅 스크립트 재작성

--- setup.sh
+++ setup.sh
@@ -4,12 +4,10 @@
 #### + Redis Cache ####
 
 # install Docker on server
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository \
-"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install docker-ce
-apt-get install docker-compose
+sudo apt install update
+sudo apt-get install docker.io
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 #remove all containers - by wonseok (2017/01/15)
  docker rm -f $(docker ps -a -q)
