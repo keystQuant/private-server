2fb48d9ad9993c2014a42351f77e7865148feca0
1 <ppark9553@gmail.com>
2018-07-19 09:33:58 +0900

    업데이트 스크립트 수정

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 #### + Redis Cache ####
 
 # install Docker on server
-sudo apt install update
+sudo apt-get update
 sudo apt-get install docker.io
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
